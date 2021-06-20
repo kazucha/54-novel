8c67b89290cfadf55e3e0067461b2b4cc72ad80f
kazucha <slyskr.crs@gmail.com>
2021-06-20 17:07:53 +0900

    modified metadata

--- cms/next/src/pages/_document.tsx
+++ cms/next/src/pages/_document.tsx
@@ -11,26 +11,26 @@ export default class Layout extends Document {
     return (
       <Html>
         <Head>
-          <title>54字の物語ジェネレーター #54字の文学賞</title>
+          <title>#この１年でやってみた</title>
           <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,shrink-to-fit=no" />
-          <meta name="description" content="54字ぴったりの物語をつくりましょう。" />
-          <meta name="keywords" content="54字の物語,#54字の文学賞,氏田雄介" />
-          <meta name="author" content="氏田雄介" />
+          <meta name="description" content="あなたがこの１年でやってみたことを書いてみましょう。" />
+          <meta name="keywords" content="#この１年でやってみた" />
+          <meta name="author" content="" />
           <meta name="generator" content="microCMS" />
-          <meta property="og:title" content="54字の物語ジェネレーター #54字の物語" />
+          <meta property="og:title" content="#この１年でやってみた" />
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://54.ujiqn.com" />
-          <meta property="og:image" content="https://54.ujiqn.com/ogp.png" />
+          <meta property="og:url" content="https://my-this-year.vercel.app/" />
+          <meta property="og:image" content="https://my-this-year.vercel.app/ogp.png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
-          <meta property="og:site_name" content="54字の物語ジェネレーター #54字の物語" />
-          <meta property="og:description" content="54字ぴったりの物語をつくりましょう。" />
+          <meta property="og:site_name" content="#この１年でやってみた" />
+          <meta property="og:description" content="あなたがこの１年でやってみたことを書いてみましょう。" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="54字の物語ジェネレーター #54字の物語" />
-          <meta name="twitter:description" content="54字ぴったりの物語をつくりましょう。" />
-          <meta name="twitter:image:src" content="https://54.ujiqn.com/ogp.png" />
+          <meta name="twitter:title" content="#この１年でやってみた" />
+          <meta name="twitter:description" content="あなたがこの１年でやってみたことを書いてみましょう。" />
+          <meta name="twitter:image:src" content="https://my-this-year.vercel.app/ogp.png" />
           <meta name="format-detection" content="telephone=no" />
-          <link rel="shortcut icon" href="https://54.ujiqn.com/favicon.ico" />
+          <link rel="shortcut icon" href="https://my-this-year.vercel.app/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap" rel="stylesheet" />
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-VS860CVJFF"></script>
