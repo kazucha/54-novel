7b4d3d4a62faefedd5b7e9a2745c1c4adf58b1fd
kazucha <slyskr.crs@gmail.com>
2021-06-27 20:30:50 +0900

    fix

--- cms/next/src/pages/index.tsx
+++ cms/next/src/pages/index.tsx
@@ -131,7 +131,7 @@ export default function IndexPage() {
               //subCtx.fillText(str, 0, 0);//context.fillText(text, x, y [, maxWidth ] ) …… 塗りつぶしのテキストを指定座標に描画する
               subCtx.fillText(str, 0, 0);//context.fillText(text, x, y [, maxWidth ] ) …… 塗りつぶしのテキストを指定座標に描画する
             subCtx.restore();
-            ctx.drawImage(subCanvas, getX, getY(i)); //文字の描画
+            ctx.drawImage(subCanvas, getX(i), getY(i)); //文字の描画
           }
         }
       ctx.restore();
