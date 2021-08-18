de6c191e30c6b33b6750550c21974c8de3f0944e
kazucha <slyskr.crs@gmail.com>
2021-08-19 02:16:39 +0900

    additional req

--- cms/next/src/pages/_document.tsx
+++ cms/next/src/pages/_document.tsx
@@ -34,15 +34,6 @@ export default class Layout extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap" rel="stylesheet" />
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-VS860CVJFF"></script>
-          <script
-            dangerouslySetInnerHTML={{__html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-VS860CVJFF');
-            `}}
-          />
         </Head>
         <body>
         <div className="white-header"> </div>
